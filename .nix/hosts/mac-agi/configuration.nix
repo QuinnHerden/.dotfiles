f2b7d9bb9fb24c30c0d552c07c7f08b8f5ec384f
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-03-07 02:21:42 -0500

    temp disable darwin-specific packages

--- .nix/hosts/mac-agi/configuration.nix
+++ .nix/hosts/mac-agi/configuration.nix
@@ -13,7 +13,7 @@
   system.stateVersion = 4; # $ darwin-rebuild changelog
   nixpkgs.hostPlatform = "x86_64-darwin";
 
-  darwinPackages.enable = true;
+  #darwinPackages.enable = true;
 
   ids.gids.nixbld = 350;
 
