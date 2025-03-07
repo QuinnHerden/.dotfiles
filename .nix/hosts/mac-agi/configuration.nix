0c85051da99f36c0d393f9c2fcf47410dbf253b3
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-03-07 02:15:58 -0500

    clean up

--- .nix/hosts/mac-agi/configuration.nix
+++ .nix/hosts/mac-agi/configuration.nix
@@ -4,31 +4,22 @@
 }:
 
 {
-  # Docs: https://daiderd.com/nix-darwin/manual/index.html
 
-  # Necessary for using flakes on this system.
   nix.settings.experimental-features = [
     "flakes"
     "nix-command"
   ];
   
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
-
-  # The platform the configuration will be used on.
-  # If you're on an Intel system, replace with "x86_64-darwin"
+  system.stateVersion = 4; # $ darwin-rebuild changelog
   nixpkgs.hostPlatform = "x86_64-darwin";
-  
+
   darwinPackages.enable = true;
+
+  ids.gids.nixbld = 350;
 
   driverUser = {
     enable = true;
     userName = "quinnherden";
   };
-
-  # Set the configured group ID to match the actual value
-  # Possible cause: trying a new Nix installation with a pre-existing installation
-  ids.gids.nixbld = 350;
 
 }
