a6be41a587ec20ba571d2dbaa6095b0a191d6b56
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-03-10 11:49:39 -0400

    begin to modularize home manager

--- .nix/hosts/mac-papi/default.nix
+++ .nix/hosts/mac-papi/default.nix
@@ -6,23 +6,13 @@
 }:
 
 {
-  imports = [
-    inputs.home-manager.darwinModules.default
-  ];
-
   nix.settings.experimental-features = [
     "flakes"
     "nix-command"
   ];
   system.stateVersion = 4; # $ darwin-rebuild changelog
-  
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  darwinPackages.enable = true;
-  baseBrews.enable = true;
-  extraBrews.enable = true;
-  experimentalBrews.enable = true;
-  
   hostname = {
     enable = true;
     name = "mac-papi";
@@ -33,8 +23,13 @@
     home = "/Users/quinnherden";
   };
 
-  home-manager.users.quinnherden.imports = [
-    ./home.nix
-  ];
+  homeTest = {
+    enable = true;
+    name = "quinnherden";
+  };
 
+  darwinPackages.enable = true;
+  baseBrews.enable = true;
+  extraBrews.enable = true;
+  experimentalBrews.enable = true;
 }
