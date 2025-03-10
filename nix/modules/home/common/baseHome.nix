5c6695e5cec467f2eeb89f6148acd216a2302873
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-03-10 19:44:10 -0400

    possibly change xbb options

--- nix/modules/home/common/baseHome.nix
+++ nix/modules/home/common/baseHome.nix
@@ -129,6 +129,10 @@
         enableZshIntegration = true;
       };
 
+      home.keyboard.options = [
+        "ctrl:swapcaps"
+      ];
+
       # The state version is required and should stay at the version you
       # originally installed.
       home.stateVersion = "24.11";
