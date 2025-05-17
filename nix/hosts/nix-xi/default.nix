fa7ee5c4cc585acf3bf7fba8bf44c900da0a06c2
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-05-17 08:59:29 -0400

    clean up working Reddit advice

--- nix/hosts/nix-xi/default.nix
+++ nix/hosts/nix-xi/default.nix
@@ -37,9 +37,6 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
-
-
-
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
@@ -138,6 +135,11 @@
     home = "/home/quinnherden";
   };
 
+  # Install Packages
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
   #users.groups.ollama = { };
 
   #users.users.ollama = {
@@ -154,78 +156,28 @@
   # Allow unfree packages
   #nixpkgs.config.allowUnfree = true;
 
-  # Enable OpenGL
-  #hardware.graphics = {
-    #enable = true;
-  #};
-
-  # Load nvidia driver for Xorg and Wayland
-  #services.xserver.videoDrivers = ["nvidia"];
-
-  #hardware.nvidia = {
-
-    # Modesetting is required.
-    #modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    #powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    #powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    #open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    #nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    #package = config.boot.kernelPackages.nvidiaPackages.stable;
-  #};
-
-  # Install Packages
-  environment.systemPackages = with pkgs; [
-    #ollama-cuda
-    vim
-  ];
+  # Tailscale (wireguard)
   services.tailscale.enable = true;
 
-  #systemd.services.ollama = {
-    #enable = true;
-    #description = "Ollama Server";
-    #after = [ "network.target" ];
-    #wantedBy = [ "multi-user.target" ];
-    #environment = {
-      #CUDA_VISIBLE_DEVICES = "1,2";
-    #};
-    #serviceConfig = {
-      #ExecStart = "${pkgs.ollama}/bin/ollama serve";
-      #Restart = "always";
-      #User = "ollama";
-      #WorkingDirectory = "/var/lib/ollama";
-    #};
-  #};
+  # Ollama (LLMs)
+  services.ollama = {
+    enable = true;
+    acceleration="cuda";
+  };
 
-  # reddit advice
-  services.ollama = { enable = true; acceleration="cuda"; };
-
-  # Reboot unless nvidia-smi ok 
+  ### Set Up Graphics Card for Generative AI ###
+  # Enable OpenGL
   hardware.graphics.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ]; 
-  hardware.nvidia = { open = false; modesetting.enable = true; }; 
-  #nixpkgs.config.allowUnfree = true; 
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+  };
+
   nixpkgs.config.allowUnfreePredicate = pkg: 
   builtins.elem (lib.getName pkg) [ "cuda_cccl" "cuda_cudart" "cuda_nvcc" "libcublas" "nvidia-settings" "nvidia-x11" ]; 
-  #nixpkgs.config.nvidia.acceptLicense = true;
 }
 
