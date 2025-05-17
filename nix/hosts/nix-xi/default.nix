46602747f1a0835af0a4e0477326c60b0523d18a
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-05-17 08:45:07 -0400

    testing reddit comment advice

--- nix/hosts/nix-xi/default.nix
+++ nix/hosts/nix-xi/default.nix
@@ -138,44 +138,44 @@
     home = "/home/quinnherden";
   };
 
-  users.groups.ollama = { };
+  #users.groups.ollama = { };
 
-  users.users.ollama = {
-    isSystemUser = true;
-    description = "Ollama service user";
-    group = "ollama";
-    home = "/var/lib/ollama";
-    createHome = true;
-  };
+  #users.users.ollama = {
+    #isSystemUser = true;
+    #description = "Ollama service user";
+    #group = "ollama";
+    #home = "/var/lib/ollama";
+    #createHome = true;
+  #};
 
   # Configure bootloader device
   boot.loader.grub.device = "/dev/nvme0n1";
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  #nixpkgs.config.allowUnfree = true;
 
   # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
+  #hardware.graphics = {
+    #enable = true;
+  #};
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  #services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
+  #hardware.nvidia = {
 
     # Modesetting is required.
-    modesetting.enable = true;
+    #modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+    #powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    #powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -183,36 +183,49 @@
     # supported GPUs is at:
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
-    open = false;
+    #open = false;
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+    #nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
+  #};
 
   # Install Packages
   environment.systemPackages = with pkgs; [
-    ollama-cuda
+    #ollama-cuda
     vim
   ];
   services.tailscale.enable = true;
 
-  systemd.services.ollama = {
-    enable = true;
-    description = "Ollama Server";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    environment = {
-      CUDA_VISIBLE_DEVICES = "1,2";
-    };
-    serviceConfig = {
-      ExecStart = "${pkgs.ollama}/bin/ollama serve";
-      Restart = "always";
-      User = "ollama";
-      WorkingDirectory = "/var/lib/ollama";
-    };
-  };
+  #systemd.services.ollama = {
+    #enable = true;
+    #description = "Ollama Server";
+    #after = [ "network.target" ];
+    #wantedBy = [ "multi-user.target" ];
+    #environment = {
+      #CUDA_VISIBLE_DEVICES = "1,2";
+    #};
+    #serviceConfig = {
+      #ExecStart = "${pkgs.ollama}/bin/ollama serve";
+      #Restart = "always";
+      #User = "ollama";
+      #WorkingDirectory = "/var/lib/ollama";
+    #};
+  #};
+
+  # reddit advice
+  services.ollama = { enable = true; acceleration="cuda"; };
+
+  # Reboot unless nvidia-smi ok 
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ]; 
+  hardware.nvidia = { open = false; modesetting.enable = true; }; 
+  #nixpkgs.config.allowUnfree = true; 
+  nixpkgs.config.allowUnfreePredicate = pkg: 
+  builtins.elem (lib.getName pkg) [ "cuda_cccl" "cuda_cudart" "cuda_nvcc" "libcublas" "nvidia-settings" "nvidia-x11" ]; 
+  #nixpkgs.config.nvidia.acceptLicense = true;
 }
+
