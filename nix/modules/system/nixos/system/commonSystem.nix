d24543497333293f3b9483af5bc05b34fa9987d3
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-13 22:35:27 -0400

    rm i3

--- nix/modules/system/nixos/system/commonSystem.nix
+++ nix/modules/system/nixos/system/commonSystem.nix
@@ -19,29 +19,6 @@
       efi.canTouchEfiVariables = true;
     };
 
-    services.xserver = {
-      enable = true;
-
-      desktopManager = {
-        xterm.enable = false;
-      };
-    
-      displayManager = {
-        lightdm.enable = true; # pick one DM
-        defaultSession = "none+i3";
-      };
-
-      windowManager.i3 = {
-        enable = true;
-        extraPackages = with pkgs; [
-          dmenu #application launcher most people use
-          i3status # gives you the default i3 status bar
-          i3blocks #if you are planning on using i3blocks over i3status
-      ];
-      };
-    };
-    
-    programs.i3lock.enable = true; #default i3 screen locker
   };
 
 }
