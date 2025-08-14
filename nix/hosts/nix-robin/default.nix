e2e48b7399089e7b28e731967374df030f420449
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-14 00:23:20 -0400

    add i3 config

--- nix/hosts/nix-robin/default.nix
+++ nix/hosts/nix-robin/default.nix
@@ -43,6 +43,15 @@
     };
 
     windowManager.i3.enable = true;
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3blocks #if you are planning on using i3blocks over i3status
+     ];
+    };
   };
+
+  programs.i3lock.enable = true; #default i3 screen locker
 }
 
