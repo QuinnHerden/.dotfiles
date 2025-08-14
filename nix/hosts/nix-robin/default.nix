8997002c4ea3a6c4cbf9e8be33b7784a3f2ce3ac
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-14 00:25:44 -0400

    clean up i3

--- nix/hosts/nix-robin/default.nix
+++ nix/hosts/nix-robin/default.nix
@@ -42,16 +42,7 @@
       xterm.enable = false;
     };
 
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3blocks #if you are planning on using i3blocks over i3status
-     ];
-    };
+    windowManager.i3.enable = true;
   };
-
-  programs.i3lock.enable = true; #default i3 screen locker
 }
 
