671138c385b00389082a21cd5e71d98bdde993d6
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-13 21:10:31 -0400

    attempt fix i3

--- nix/modules/system/nixos/system/commonSystem.nix
+++ nix/modules/system/nixos/system/commonSystem.nix
@@ -18,28 +18,29 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-  };
 
-  services.xserver = {
-    enable = true;
-
-    desktopManager = {
-      xterm.enable = false;
-    };
-   
-    displayManager = {
-        defaultSession = "none+i3";
-    };
-
-    windowManager.i3 = {
+    services.xserver = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3blocks #if you are planning on using i3blocks over i3status
-     ];
+
+      desktopManager = {
+        xterm.enable = false;
+      };
+    
+      displayManager = {
+          defaultSession = "none+i3";
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu #application launcher most people use
+          i3status # gives you the default i3 status bar
+          i3blocks #if you are planning on using i3blocks over i3status
+      ];
+      };
     };
+    
+    programs.i3lock.enable = true; #default i3 screen locker
   };
-  programs.i3lock.enable = true; #default i3 screen locker
 
 }
