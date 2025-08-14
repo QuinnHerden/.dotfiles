4b7912915aad4348f7c42d72597858604095e8d6
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-14 12:01:43 -0400

    trying to fix touchpad

--- nix/hosts/nix-robin/default.nix
+++ nix/hosts/nix-robin/default.nix
@@ -29,18 +29,35 @@
 
   networking.networkmanager.enable = true;
 
+  services.libinput = {
+    enable = true;
+  };
+
   services.xserver = {
     enable = true;
 
     xkb.options = "ctrl:swapcaps";
 
+    xserver.config = ''
+      Section "InputClass"
+          Identifier "ELAN Touchpad"
+          MatchIsTouchpad "on"
+          Driver "libinput"
+          Option "Tapping" "on"
+          Option "NaturalScrolling" "true"
+          Option "ScrollMethod" "twofinger"
+      EndSection
+    '';
+
     desktopManager = {
       xterm.enable = false;
     };
 
-    windowManager.i3.enable = true;
+    windowManager = {
+      i3.enable = true;
+    };
   };
-
+  
   services.displayManager = {
     defaultSession = "none+i3";
   };
