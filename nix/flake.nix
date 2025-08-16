c17b7a3a28382e561d9f04ed68e5946ef17c7b9b
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-16 03:32:34 +0000

    attempting nix channel upgrade

--- nix/flake.nix
+++ nix/flake.nix
@@ -12,7 +12,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
