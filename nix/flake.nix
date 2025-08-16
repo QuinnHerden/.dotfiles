373b6cc607fa1b89168b1ed22def5dfa176d1016
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-15 23:44:47 -0400

    finish channel upgrade

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,7 +7,7 @@
     };
     
     darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
