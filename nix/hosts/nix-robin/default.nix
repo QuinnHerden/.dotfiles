16a888c8a02e1ead3d347071d2567e51c7a2ef4d
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-18 17:35:14 +0000

    extract redshift

--- nix/hosts/nix-robin/default.nix
+++ nix/hosts/nix-robin/default.nix
@@ -40,6 +40,7 @@
   keyd.enable = true;
   libinput.enable = true;
   i3.enable = true;
+  redshift.enable = true;
 
 
   #######################################
@@ -52,20 +53,6 @@
  
   #######################################
 
-
-  location.provider = "geoclue2";
-
-  services.redshift = {
-    enable = true;
-    brightness = {
-      day = "1";
-      night = "1";
-    };
-    temperature = {
-      day = 5500;
-      night = 3700;
-    };
-  };
 
   virtualisation.docker = {
     enable = true;
