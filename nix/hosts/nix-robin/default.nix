c7b1532a248966e03341af0a9d93d0ef8e6c76eb
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-18 17:54:53 +0000

    extract vpn

--- nix/hosts/nix-robin/default.nix
+++ nix/hosts/nix-robin/default.nix
@@ -32,9 +32,10 @@
 
   quinnherdenUser.enable = true;
 
-  openssh.enable = true;
-
   wifi.enable = true;
+  openssh.enable = true;
+  vpn-us-ga-285.enable = true;
+
   bluetooth.enable = true;
 
   keyd.enable = true;
@@ -47,7 +48,6 @@
 
   linuxPackages.enable = true;
 
-
   #######################################
   # fix touchpad error:
   # "elan_i2c invalid report id data (1)"
@@ -55,52 +55,7 @@
   boot.extraModprobeConfig = ''
     blacklist elan_i2c
   '';
- 
   #######################################
-
-  ### VPN ###
-
-  networking.wg-quick.interfaces = let
-    # [Interface] > PrivateKey
-    int_private_key_file = "/etc/nix-robin-pvpn.key";
-
-    # [Interface] > Address
-    int_cidr_ipv4 = "10.2.0.2/32";
-
-    # [Peer] > PublicKey
-    peer_public_key = "vsquyHHSbv76cOqCMZCREGur05Mp5XM0lbCAzrGDs2w=";
-
-    # [Peer] > AllowedIPs
-    peer_allowed_cidr_ipv4 = "0.0.0.0/0";
-    peer_allowed_cidr_ipv6 = "::/0";
-
-    # [Peer] > Endpoint:port
-    peer_ipv4 = "149.22.94.86";
-    peer_port = 51820;
-
-  in {
-    wg0 = {
-      address = [
-        int_cidr_ipv4
-      ];
-
-      listenPort = peer_port;
-
-      privateKeyFile = int_private_key_file;
-
-      peers = [
-        {
-          allowedIPs = [
-            peer_allowed_cidr_ipv4
-            peer_allowed_cidr_ipv6
-          ];
-          publicKey = peer_public_key;
-          endpoint = "${peer_ipv4}:${toString(peer_port)}";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
 
 }
 
