7ff87c63f943d637799d67c2c9e48e0e13e96cc3
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-18 18:26:41 +0000

    clean up config

--- nix/hosts/nix-xi/default.nix
+++ nix/hosts/nix-xi/default.nix
@@ -11,25 +11,46 @@
   ];
   system.stateVersion = "24.11";
   
-  boot.loader.grub.device = "/dev/nvme0n1";
+  nix.settings.experimental-features = [
+    "flakes"
+    "nix-command"
+  ];
 
   hostname = {
     enable = true;
     name = "nix-xi";
   };
   
-  openssh.enable = true;
-
-  quinnherdenUser.enable = true;
-
-  baseHome = {
+  commonBaseHome = {
+    enable = true;
+    name = "quinnherden";
+  };
+  nixosBaseHome = {
     enable = true;
     name = "quinnherden";
   };
 
-  services.tailscale = {
-    enable = true;
-  };
+  quinnherdenUser.enable = true;
+
+  wifi.enable = true;
+  openssh.enable = true;
+  vpn-us-ga-285.enable = false;
+
+  bluetooth.enable = true;
+
+  keyd.enable = true;
+  libinput.enable = true;
+
+  i3.enable = true;
+  redshift.enable = true;
+
+  docker.enable = true;
+
+  linuxPackages.enable = true;
+
+  ### extract ###
+
+  boot.loader.grub.device = "/dev/nvme0n1";
 
   services.ollama = {
     enable = true;
