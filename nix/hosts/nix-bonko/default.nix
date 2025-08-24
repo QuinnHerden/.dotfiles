edecf9288d8121628a656c9b948a94a2409ad157
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-24 19:00:54 -0400

    adding base host config for raspberry pi 4

---
+++ nix/hosts/nix-bonko/default.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [ 
+    ./hardware-configuration.nix
+  ];
+  system.stateVersion = "24.11";
+
+  nix.settings.experimental-features = [
+    "flakes"
+    "nix-command"
+  ];
+
+  hostname = {
+    enable = true;
+    name = "nix-bonko";
+  };
+
+  commonBaseHome = {
+    enable = true;
+    name = "quinnherden";
+  };
+  nixosBaseHome = {
+    enable = true;
+    name = "quinnherden";
+  };
+
+  quinnherdenUser.enable = true;
+
+  wifi.enable = true;
+  openssh.enable = true;
+
+  keyd.enable = true;
+  libinput.enable = true;
+
+  i3.enable = true;
+  redshift.enable = true;
+
+  docker.enable = true;
+
+  linuxPackages.enable = true;
+
+}
+
