267b46c17fe991c58bc72e5eadbb9cd8d7cbc76d
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-08-24 21:36:54 -0400

    move experimental features

--- nix/hosts/mac-papi/default.nix
+++ nix/hosts/mac-papi/default.nix
@@ -7,11 +7,6 @@
   system.stateVersion = 4; # $ darwin-rebuild changelog
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  nix.settings.experimental-features = [
-    "flakes"
-    "nix-command"
-  ];
-  
   hostname = {
     enable = true;
     name = "mac-papi";
