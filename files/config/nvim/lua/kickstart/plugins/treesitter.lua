0b31ceb80b6902efc95439e189ec614906bc9a33
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2026-04-22 17:46:46 -0400

    fix lazy loading tree in nvim

--- files/config/nvim/lua/kickstart/plugins/treesitter.lua
+++ files/config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
