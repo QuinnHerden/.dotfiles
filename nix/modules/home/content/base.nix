0b9ff20328f165c80f7e1bf314579d5a45bf4f10
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2026-04-22 17:54:34 -0400

    attempt add generic linux support

--- nix/modules/home/content/base.nix
+++ nix/modules/home/content/base.nix
@@ -49,12 +49,7 @@
     autosuggestion.enable = true;
 
     envExtra = ''
-      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-      fi
-      if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
-        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-      fi
+      export PATH="$HOME/.nix-profile/bin:$PATH"
     '';
 
     oh-my-zsh = {
