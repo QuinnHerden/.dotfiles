c31423a1c9e8aa2038f178846b3fc8441759a573
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2026-04-22 18:05:54 -0400

    attempt add generic linux support

--- nix/modules/home/content/base.nix
+++ nix/modules/home/content/base.nix
@@ -81,7 +81,6 @@
       # fzf
       ff = "fzf --style full --preview 'fzf-preview.sh {}' --bind 'focus:transform-header:file --brief {}'";
 
-
       # git
       gs = "git status";
       gd = "git diff";
